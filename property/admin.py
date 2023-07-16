f46a2d3dd971d96931482509ad746d991a76d359
Michael Matasyants <michael.matasyanc@gmail.com>
2023-07-16 13:55:43 +0300

    Display owners on flat page in admin

--- property/admin.py
+++ property/admin.py
@@ -12,7 +12,8 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
     list_display = [
-        'address', 'price', 'new_building', 'construction_year', 'town'
+        'flat_owners', 'address', 'price', 'new_building',
+        'construction_year', 'town'
         ]
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
